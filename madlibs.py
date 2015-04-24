9f046f7e4cf570c0685b493c9797377d970f69a3
Hackbright Student <info@hackbrightacademy.com>
2015-04-23 18:49:15 -0700

    Created a simple madlib game using flask.

--- madlibs.py
+++ madlibs.py
@@ -7,27 +7,50 @@ from flask import Flask, render_template, request
 # Flask wants to know this to know what any imported things are relative to.
 app = Flask(__name__)
 
-# route to handle the landing page of a website.
 @app.route('/')
-def start_here():
-    return "Hi! This is the home page."
-
-# route to display a simple web page
-@app.route('/hello')
-def say_hello():
+def the_greet():
     return render_template("hello.html")
+# route to handle the landing page of a website.
+@app.route('/next')
+def show_game_form():
+    person = request.args.get("person")
+    return render_template("compliment.html",person=person)
 
-@app.route('/greet')
-def greet_person():
-    player = request.args.get("person")
 
-    AWESOMENESS = [
-        'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
-        'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+#create a function called show_game_form to route to the url path /game
+@app.route('/game')
+def game_form():
+    confirmplay = request.args.get("confirmplay")
+    if confirmplay == "True":
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
 
-    compliment = choice(AWESOMENESS)
+@app.route('/madlib')
+def show_mad_lib():
+    madlibcolor = request.args.get("madlibcolor")
+    madlibnoun = request.args.get("madlibnoun")
+    person = request.args.get("nickname")
+    madlibadjective = request.args.get("madlibadjective")
+    return render_template("madlib.html",madlibcolor=madlibcolor, madlibnoun=madlibnoun, person=person, madlibadjective=madlibadjective )
 
-    return render_template("compliment.html", person=player, compliment=compliment)
+
+# # route to display a simple web page
+# @app.route('/hello')
+# def say_hello():
+#     return render_template("hello.html")
+
+# @app.route('/greet')
+# def greet_person():
+#     player = request.args.get("person")
+ 
+    # AWESOMENESS = [
+    #     'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+    #     'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+
+    # compliment = choice(AWESOMENESS)
+
+    # # return render_template("compliment.html", person=player, compliment=compliment)
 
 
 if __name__ == '__main__':
